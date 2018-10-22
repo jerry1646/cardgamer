edb5298da75bafa0ee3ba3a99e8e75d303a57c70
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-10-22 14:25:13 +0000

    heroku

--- knexfile.js
+++ knexfile.js
@@ -24,10 +24,10 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL + '?ssl=true',
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: './db/migrations',
       tableName: 'migrations'
